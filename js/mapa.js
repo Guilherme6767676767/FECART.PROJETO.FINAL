d2900b7d4734a146547c7e09512e651c35847b37
Eduardo <e56969481@gmail.com>
2026-09-01 12:19:53 -0300

    fix: remover conflitos de css do mapa e atualizar cache buster para v2.0

--- js/mapa.js
+++ js/mapa.js
@@ -53,8 +53,8 @@
     attributionControl: true
   });
 
-  // Tile layer escuro (OpenStreetMap Dark / CartoDB Dark Matter sem restrição)
-  L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png', {
+  // Tile layer escuro (OpenStreetMap Dark Matter - sem restrição / sem marca d'água)
+  L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/">CARTO</a>',
     subdomains: 'abcd',
     maxZoom: 19
